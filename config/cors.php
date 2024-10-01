8a9c5fbe5f9bbb39112cda1e00c8c41828ee8cb5
Diallo-alpha <alphaloppecity@gmail.com>
2024-10-01 13:48:56 +0000

    correction de configuration de paiement

--- config/cors.php
+++ config/cors.php
@@ -3,10 +3,13 @@
 return [
     'paths' => ['api/*', 'login', 'logout', 'refresh'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://admirable-macaron-cbfcb1.netlify.app'],
+    'allowed_origins' => [
+        'https://admirable-macaron-cbfcb1.netlify.app',
+        'http://localhost:4200',
+    ],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization'],
+    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization', 'Accept'],
     'exposed_headers' => ['Authorization'],
     'max_age' => 0,
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
